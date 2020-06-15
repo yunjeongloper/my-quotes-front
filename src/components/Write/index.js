7ba69640871f172bb405680fb8a773302734dcd9
veronica <vero911@naver.com>
2020-06-15 22:44:10 +0900

    Add write modal with design

--- src/components/Write/index.js
+++ src/components/Write/index.js
@@ -14,7 +14,22 @@ const WriteModal = ({isShow, object, closeFn}) => {
         X
       </button>
       <div className="input-container">
-        <textarea className="letter-input" />
+        <textarea className="letter-input" id="letter-write" />
+        <div className="author-container">
+          <span>From</span>
+          <input
+            type="text"
+            className="author-input"
+            placeholder="익명의 누군가"
+          />
+        </div>
+        <div className="button-container">
+          <button
+          // onClick={() => history.push('/list')}
+          >
+            <span className="font-weight-bold">보내기</span>
+          </button>
+        </div>
       </div>
     </div>
   );
